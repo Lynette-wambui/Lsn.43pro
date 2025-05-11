d3d617aead329bece136cc7992e2dfbe8c5bc77c
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-05-11 19:40:58 +0300

    Add files via upload

---
+++ pr.py
@@ -0,0 +1,71 @@
+# Factorial of a number using a recursion
+def recur_factorial(n):
+    if n== 1:
+        return n
+    else:
+        return n*recur_factorial(n-1)
+
+num = int(input("Enter a number"))
+
+# check if the number is negative
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", num, "is", recur_factorial(num))
+
+
+
+# This function adds two numbers 
+def add(x, y):
+    return x + y
+
+# This function subtacts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+ #This function divides two numbers
+def divide(x, y):
+    return x / y
+
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2 : "))
+
+print("Sum :", add(num1, num2))
+print("Difference :", subtract(num1, num2))
+print("Product :", multiply(num1, num2))
+print("Quotient :",divide(num1, num2))
+
+# fibonacci sequence
+
+def fibonacci(n):
+    sequence = [0, 1]  # Start with the first two Fibonacci numbers
+    for _ in range(n - 2):  # Generate the next numbers until we reach n terms
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+# Get user input
+n = int(input("Enter the number of terms in the Fibonacci sequence: "))
+
+if n <= 0:
+    print("Please enter an integer.")
+elif n == 1:
+    print([0])
+else:
+    print("Fibonacci sequence up to terms:", fibonacci(n))
+
+
+
+
+
+
+
+
+
+
+
